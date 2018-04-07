276bcf993bf713eb3bb7bf80888bd4a6e6acdf87
GianlucaParilli <gluka47@hotmail.com>
2018-04-07 15:57:13 -0400

    final app working with ipads 9.2+

--- www/js/app.js
+++ www/js/app.js
@@ -156,7 +156,7 @@ app.service('$pouchdb', function($rootScope, pouchDB, $http) {
       }
     };
 
-    self.localDB = pouchDB('judges_sp18');
+    self.localDB = pouchDB('judges');
     self.localDB.sync('http://admin:starsGGCadmin@itec-gunay.duckdns.org:5984/judges_sp18', opts)
       .on('change', function(change) {
         $rootScope.$broadcast('changes');
