28899f2aa9c79bc048c5782a22fd72d389a4add7
jcweekley <jweekley@ggc.edu>
2019-10-15 16:54:50 -0400

    Jacob and Nancy's updated folder structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'home',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  { path: '', loadChildren: './login/login.module#LoginPageModule' }
 ];
 @NgModule({
   imports: [
