2160c6805f4b53b8872df33f34b30a565f521a9d
Juan Linares <jlinares1@ggc.edu>
2019-10-22 17:17:13 -0400

    Creation of searchbar and radio button function

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -4,6 +4,23 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AppService {
+  poster: any = [];
+  searchTerm: string;
+  constructor() {
+    fetch('./assets/data/poster.json')
+      .then(res => res.json())
+      .then(json => {
+        this.poster = json;
+      });
+  }
 
-  constructor() { }
+  filterItems(searchTerm) {
+    return this.poster.filter(item => {
+      return (
+        item.subject
+          .toLowerCase()
+          .indexOf(searchTerm.toString().toLowerCase()) > -1
+      );
+    });
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from 'src/app/app.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  poster: any = [];
+  items: any = [];
+  searchTerm: string;
+  constructor(private appService: AppService) { }
 
-  constructor() {}
-
+  ngOnInit() {
+    fetch('./assets/data/poster.json')
+      .then(res => res.json())
+      .then(json => {
+        this.poster = json;
+        this.items = this.poster;
+      });
+  }
+  onChangeHandler($event) {
+    if ($event.target.value === 'all') {
+      return fetch('./assets/data/poster.json')
+        .then(res => res.json())
+        .then(json => {
+          this.poster = json;
+        });
+    } else {
+      this.poster.subject = $event.target.value;
+      this.poster = this.appService.filterItems(this.poster.subject);
+    }
+  }
+  filterItems(searchTerm) {
+    return this.items.filter(item => {
+      return (
+        item.group.toLowerCase().indexOf(searchTerm.toString().toLowerCase()) >
+        -1
+      );
+    });
+  }
+  setFilteredItems() {
+    return this.poster = this.filterItems(this.searchTerm);
+  }
 }
