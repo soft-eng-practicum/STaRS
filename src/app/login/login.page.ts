71bf289befc518bc0b2106df15663aafc84957e5
jcweekley <jweekley@ggc.edu>
2019-10-22 22:01:33 -0400

    Jacob added temporary login page validation

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  username;
+  password;
+
+  constructor(public alertController: AlertController, private  router: Router) { }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      // header: 'Alert',
+      // subHeader: 'Subtitle',
+      message: 'Please enter valid login credentials.',
+      buttons: [ {
+        text: 'OK',
+        cssClass: 'icon-color'
+      }]
+    });
+    await alert.present();
+  }
 
   ngOnInit() {
+  }
+
+  loginUser(event) {
+    // event.preventDefault();
+    // const target = event.target;
+    // // console.log(target);
+    // const username = target.querySelector('#username').value;
+    // const password = target.querySelector('#password').value;
+    // console.log(username, password);
+    if (this.username === 'polaris' && this.password === 'ggc') {
+      this.router.navigateByUrl('/home/tabs/tab1');
+    } else {
+      this.presentAlert();
+    }
   }
 
 }
