547ba4aff602e7dee3df1e17112a39532ce1663c
Juan Linares <jlinares1@ggc.edu>
2019-11-16 22:33:46 -0500

    Juan improved the survey questions and incorporated an icon

--- src/app/poster-list/poster-list.page.ts
+++ src/app/poster-list/poster-list.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PouchService } from 'src/app/pouch.service';
+import { AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -9,20 +10,34 @@ import { PouchService } from 'src/app/pouch.service';
 })
 export class PosterListPage implements OnInit {
 
-  currentUser: any;
-  surveyQuestions: any = [];
-
-  constructor(private pouchService: PouchService) {
+  constructor(private pouchService: PouchService, public alertController: AlertController) {
     this.currentUser = this.pouchService.globalUser;
     this.surveyQuestions = this.pouchService.surveyQuestions;
   }
 
-  ngOnInit() {
-  }
-
+  currentUser: any;
+  surveyQuestions: any = [];
+  controller: any;
+  question: any = [];
+  selectRadioGroup: any;
   // segmentChanged(e) {
   //   console.info(e.value.);
   // }
 
+  ngOnInit() {
+  }
+  async showAlert(index) {
+    this.question = this.surveyQuestions[index - 1];
+    const alert = await this.alertController.create({
+      header: this.question.information,
+      subHeader: this.question.additional,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
 
+  radioSelect(event){
+    this.selectRadioGroup = event.detail.value;
+    console.log(this.selectRadioGroup);
+  }
 }
